72bc59a5acf0e2cbaf9b1c84bc7011e67d3d73d3
NOBertram1 <bertramokwudili@gmail.com>
2022-03-11 21:37:46 +0100

    assign variable + or -

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Receive a random number and print the number
+ * Description: Receive a random number and printout the number
  * and if it is positive, negative, or zero
  * Return: 0
  */
