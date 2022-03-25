bc65cb80d97a6a8349de08772f12d25b99a37b15
NOBertram1 <bertramokwudili@gmail.com>
2022-03-25 05:37:42 +0100

    print lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,13 +7,14 @@
 
 void print_alphabet(void)
 {
-   	char c = 'a';
+    char c = 'a';
 
 	while (c <= 'z')
 	{
-		_putchar(c);
+        _putchar(c);
 		c += 1;
 
 	}
-	_putchar('\n'); 
+    
+    _putchar('\n'); 
 }
