d6c1fff55641f78aa7e009119edf6fd665c8cbdb
NOBertram1 <bertramokwudili@gmail.com>
2022-03-31 08:35:29 +0100

    find exponential with recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - a function that returns
+ * the value of x raised to the power of y.
+ * @x: number
+ * @y: power
+ *
+ * Return: The result or -1 for error
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y));
+}
