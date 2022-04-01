f2bd651db8a2871f7093d237cb14870cab162efe
NOBertram1 <bertramokwudili@gmail.com>
2022-04-01 05:42:55 +0100

    find squareroot with recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,19 +3,23 @@
 /**
  * _sqrt_recursion - return the natural square root of a number
  * @n: int number
- * Return: return -1 if no natural square root, Else return natural
+ * Return: If no natural square root, return -1. Else return natural
  * square root
- * sqrtd - function to solve _sqrt_recursion
- * @c: number to determine if square root
- * @i: incrementer to compare against `c`
  */
 
 int _sqrt_recursion(int n)
 {
-	return (sqrtd(n, 1));
+	return (halp(n, 1));
 }
 
-int sqrtd(int c, int i)
+/**
+ * halp - helper function to solve _sqrt_recursion
+ * @c: number to determine if square root
+ * @i: incrementer to compare against `c`
+ * Return: square root if natural square root, or -1 if none found
+ */
+
+int halp(int c, int i)
 {
 	int square;
 
@@ -23,7 +27,7 @@ int sqrtd(int c, int i)
 	if (square == c)
 		return (i);
 	else if (square < c)
-		return (sqrtd(c, i + 1));
+		return (halp(c, i + 1));
 	else
 		return (-1);
 }
